5916a9fe1c6f27f7a1217d8ef124fd467245ef2c
Vamshi Krishna <guduru.vkrishna.civ10@itbhu.ac.in>
2017-12-18 14:27:20 +0530

    Initial Commit

---
+++ FacialContour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacialContour
+//
+//  Created by Vamshi Krishna on 18/12/17.
+//  Copyright © 2017 Vamshi Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
